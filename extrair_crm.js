33a6ec08a2842998758ba8e67e226a535f18e323
matheus.freitas <matheus.freitas@marfoods.com.br>
2026-07-23 15:41:57 -0300

    feat: adiciona Anna (1571) aos vendedores televendas

--- extrair_crm.js
+++ extrair_crm.js
@@ -6,8 +6,8 @@ var BASE = BASE_DIR + '_bases\\base_8026_2026.xlsx';
 var PAINEL = BASE_DIR + 'dashboards\\crm-recuperacao\\Painel CRM - Junho.xlsx';
 var OUT_DIR = BASE_DIR + 'dashboards\\crm-recuperacao\\';
 
-var ACTIVE = ['1596', '1464', '1211', '1429', '9886', '1624'];
-var NOMES = { '1596': 'Ariane', '1464': 'Camila', '1211': 'Cristielen', '1429': 'Natália', '9886': 'Tatiana', '1624': 'Tatiana' };
+var ACTIVE = ['1596', '1464', '1211', '1429', '9886', '1624', '1571'];
+var NOMES = { '1596': 'Ariane', '1464': 'Camila', '1211': 'Cristielen', '1429': 'Natália', '9886': 'Tatiana', '1624': 'Tatiana', '1571': 'Anna' };
 var NOMES_REVERSE = { 'Ariane': '1596', 'Camila': '1464', 'Cristielen': '1211', 'Natália': '1429', 'Natalia': '1429', 'Tatiana': '9886' };
 var MES_NOMES = { 1: 'Janeiro', 2: 'Fevereiro', 3: 'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho', 7: 'Julho', 8: 'Agosto', 9: 'Setembro', 10: 'Outubro', 11: 'Novembro', 12: 'Dezembro' };
 var MES_NUM = { 'Janeiro': 1, 'Fevereiro': 2, 'Março': 3, 'Abril': 4, 'Maio': 5, 'Junho': 6, 'Julho': 7, 'Agosto': 8, 'Setembro': 9, 'Outubro': 10, 'Novembro': 11, 'Dezembro': 12 };

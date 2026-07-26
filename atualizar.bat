b0001826a56c1a96693e0c2e8191c8b46eecaf73
matheus.freitas <matheus.freitas@marfoods.com.br>
2026-07-26 19:40:33 -0300

    chore: add resumo to atualizar.bat, remove Novo Arquivo button
    - atualizar.bat: added step [3/4] with summary (per-month totals + client count)
    - index.html: removed Novo Arquivo button and novoArquivo() function
    - All 4 btnNovoArquivo references removed

--- atualizar.bat
+++ atualizar.bat
@@ -9,7 +9,7 @@ set "BASEDIR=C:\Users\mar\OneDrive - SPADER DISTRIBUIDORA DE ALIMENTOS L\Area de
 set "CAMDIR=%BASEDIR%\dashboards\crm-recuperacao"
 set "SCRIPTDIR=%~dp0"
 
-echo [1/3] Extraindo dados da base...
+echo [1/4] Extraindo dados da base...
 node "%SCRIPTDIR%extrair_crm.js"
 if %ERRORLEVEL% neq 0 (
     echo ERRO ao extrair dados!
@@ -19,11 +19,15 @@ if %ERRORLEVEL% neq 0 (
 echo OK!
 echo.
 
-echo [2/3] Enviando para GitHub...
+echo [2/4] Enviando para GitHub...
 git add data.json
 git commit -m "feat: atualizacao crm recuperacao %date% %time%" 2>nul
 git push
 echo.
 
-echo [3/3] Dashboard atualizado!
+echo [3/4] Resumo:
+node -e "var d=require('%CAMDIR%\\data.json');var fc=d.data.faturamentoClientes;var mt={};var mc={};fc.forEach(function(c){if(!mt[c.mes])mt[c.mes]=0;if(!mc[c.mes])mc[c.mes]={};mt[c.mes]+=c.valor;mc[c.mes][c.cliente]=1;});console.log('  Abril: R\$ '+(mt.Abril||0).toFixed(2)+' | '+Object.keys(mc.Abril||{}).length+' clientes');console.log('  Maio: R\$ '+(mt.Maio||0).toFixed(2)+' | '+Object.keys(mc.Maio||{}).length+' clientes');console.log('  Junho: R\$ '+(mt.Junho||0).toFixed(2)+' | '+Object.keys(mc.Junho||{}).length+' clientes');console.log('  Julho: R\$ '+(mt.Julho||0).toFixed(2)+' | '+Object.keys(mc.Julho||{}).length+' clientes');var t=fc.reduce(function(s,c){return s+c.valor;},0);console.log('  Total acumulado: R\$ '+t.toFixed(2));console.log('  Total clientes: '+Object.keys(d.data.clientesDetalhes).length);"
+echo.
+
+echo [4/4] Dashboard atualizado!
 pause
